4642190c0d7f4bce2d2ee73d7db643cb3b00085e
Bahaa ALI ALHabil <bahaa.ali.m.alhabil@gmail.com>
2024-12-24 01:20:14 +0300

    new Hero

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -178,7 +178,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("cde40e9cffbcd693")
+/******/ 		__webpack_require__.h = () => ("7468942b37f515b5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,32 @@
+import Link from "next/link";
+
 export default function Hero() {
   return (
-    <section>
+    <section className="py-24">
       <div className="container">
-        <div>أكثر من 100ألف مستخدم مسجل لدينا</div>
+        <div className="flex justify-center">
+          <div className="inline-flex items-center justify-center py-1 px-3 bg-gradient-to-l from-purple-400 to-purple-600 rounded-full text-neutral-950 font-semibold">
+            أكثر من 100ألف مستخدم مسجل لدينا
+          </div>
+        </div>
         <h1>وكـالـة الأصـيـل</h1>
         <p>
           نحن نساعدك على أن تكتشف موهبتك أو أن تطور أعمالك من خلال تقديم أفضل
           الاستشارات الاحترافية التي تساعدك على الارتقاء بمحتواك عبر منصة{" "}
         </p>
+
+        <Link
+          href="/signup"
+          style={{
+            color: "white",
+            backgroundColor: "black",
+            padding: "10px 20px",
+            textDecoration: "none",
+            borderRadius: "5px",
+          }}
+        >
+          أنظم الان
+        </Link>
       </div>
     </section>
   );
